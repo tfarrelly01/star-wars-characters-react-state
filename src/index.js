b9ff0bba8c62a42ef59937c4e63469a59f473c28
Tony Farrelly <tfarrelly01@gmail.com>
2022-07-26 12:47:08 +0100

    01-Data Fetching and useEffect

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -6,11 +6,27 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import CharacterList from './CharacterList';
 
 import dummyData from './dummy-data';
+import endpoint from './endpoint';
 
 import './styles.scss';
 
 const Application = () => {
-  const [characters, setCharacters] = useState(dummyData);
+
+  console.log(endpoint);
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetch(endpoint + '/characters')
+    // fetch(`${endpoint}/characters`)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        // setCharacters(Object.values(response.characters));
+        setCharacters(response.characters);
+      })
+      .catch(console.error);
+  }, []);
+
 
   return (
     <div className="Application">
